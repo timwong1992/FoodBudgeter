85c3d29edc2c1483992b98bafdef21f011ebd002
Tim Wong <tim.wong1992@gmail.com>
2013-05-09 02:28:57 -0400

    modified gitignore, updated documentation

---
+++ FoodBudgeter/FoodBudgeter/model/ModelManager.h
@@ -0,0 +1,79 @@
+//
+//  ModelManager.h
+//  FoodBudgeter
+//
+//  Created by Tim Wong on 5/3/13.
+//  Copyright (c) 2013 Tim Wong, Akia Vongdara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AppDelegate.h"
+#import "DBManager.h"
+#import "Item.h"
+#import "RecipeItem.h"
+#import "PurchasedItem.h"
+#import "GroceryItem.h"
+
+@class ModelManager;
+
+@protocol ModelDelegate <NSObject>
+
+@optional
+- (void)databaseHasBeenUpdated;
+
+@end
+
+@interface ModelManager : NSObject
+
+@property (nonatomic, assign) id<DBDelegate> logDelegate;
+@property (nonatomic, assign) id<DBDelegate> viewDelegate;
+
+/*
+ Runs a query and returns its status.
+ */
+- (int) runQuery:(const char *)query
+      onDatabase:(sqlite3 *)database
+withErrorMessage:(char *)errMsg;
+
+/*
+ Given item data, adds it to the database. Returns false if the item is a duplicate or the database is not changed.
+ */
+- (BOOL)addItem:(NSString*)itemName
+       withType:(int)itemType
+withIngredients:(NSMutableArray*)ingredients
+       withCost:(double)itemCost;
+
+/*
+ Given ingredient data, adds it to the database. Returns false if the ingredient is a duplicate or the database is not changed.
+ */
+- (BOOL)addIngredient:(NSString *)ingredientName
+             withCost:(double)ingredientCost;
+
+/*
+ Returns the number of items in the database.
+ */
+- (int)numItemsInDatabase;
+
+/*
+ Returns an array of item names from the database.
+ */
+- (NSMutableArray*)itemsInDatabase;
+
+/*
+ Given item data, remove it from database. Returns false if the database is not changed.
+ */
+- (BOOL)removeItem:(NSString*)itemName;
+
+/*
+ Given ingredient data, remove it from the database. Returns false if the database is not changed.
+ */
+- (BOOL)removeIngredient:(NSString *)ingredientName;
+
+
+/*
+ Given a name, searches for the item in the database and returns its unique itemID.
+ Returns -1 if the item is not found.
+ */
+- (int)itemID:(NSString *)itemName;
+
+@end

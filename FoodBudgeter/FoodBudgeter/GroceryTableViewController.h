9b4ac1baf0e04d15be7c65b5a97ec600a417a197
Student <student@sl2670-m03-2.local>
2013-05-14 20:33:48 -0400

    hi

---
+++ FoodBudgeter/FoodBudgeter/GroceryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroceryTableViewController.h
+//  FoodBudgeter
+//
+//  Created by Student on 5/14/13.
+//  Copyright (c) 2013 Tim Wong, Akia Vongdara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroceryTableViewController : UITableViewController
+
+@end

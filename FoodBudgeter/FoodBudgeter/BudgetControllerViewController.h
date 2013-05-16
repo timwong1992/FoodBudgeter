c02e3aa5bb81d3c050f9360196cdb20504281f10
Student <tim.wong1992@gmail.com>
2013-05-15 21:49:21 -0400

    initial commit of budget page

---
+++ FoodBudgeter/FoodBudgeter/BudgetControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  BudgetControllerViewController.h
+//  FoodBudgeter
+//
+//  Created by Student on 5/15/13.
+//  Copyright (c) 2013 Tim Wong, Akia Vongdara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BudgetControllerViewController : UIViewController
+
+@end
---
+++ FoodBudgeter/FoodBudgeter/BudgetControllerViewController.m
@@ -0,0 +1,38 @@
+//
+//  BudgetControllerViewController.m
+//  FoodBudgeter
+//
+//  Created by Student on 5/15/13.
+//  Copyright (c) 2013 Tim Wong, Akia Vongdara. All rights reserved.
+//
+
+#import "BudgetControllerViewController.h"
+
+@interface BudgetControllerViewController ()
+
+@end
+
+@implementation BudgetControllerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
